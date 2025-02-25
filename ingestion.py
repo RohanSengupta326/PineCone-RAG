8798fd1db7b396082ebcddd86ff510eb92dad354
RohanSengupta326 <rohansengupta326@gmail.com>
2025-02-25 18:05:41 +0530

    -

--- ingestion.py
+++ ingestion.py
@@ -16,7 +16,6 @@ from dotenv import load_dotenv
 from langchain_community.document_loaders import TextLoader
 from langchain_ollama import OllamaEmbeddings
 from langchain_text_splitters import CharacterTextSplitter
-from langchain_openai import OpenAIEmbeddings
 from langchain_pinecone import PineconeVectorStore
 
 load_dotenv()
